31131c386e725fafe5f7ecad752fab74428834e2
Yacine Berrada <yacineberrada@gmail.com>
2019-05-21 23:47:33 +0200

    First working graphical version, still to be refined

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,30 +1,105 @@
-var rover = {};
-rover.direction="N";
-rover.x=0;
-rover.y=0;
-rover.travelLog=[[0,0]];
+var rover1 = {};
+var rover2 = {};
+var rover3 = {};
+
+rover1.direction="N";
+rover1.x=0;
+rover1.y=0;
+rover1.travelLog=[[0,0]];
+rover1.color="black";
+
+rover2.direction="W";
+rover2.x=4;
+rover2.y=5;
+rover2.travelLog=[[4,5]];
+rover2.color="red";
+
+rover3.direction="W";
+rover3.x=8;
+rover3.y=5;
+rover3.travelLog=[[4,5]];
+rover3.color="green";
+
+var obstacles = [[4,0], [5,9], [2,6]];
 
 var x_boundary=10;
 var y_boundary=10;
 
+rover=rover1;
 
+
+document.addEventListener('keydown', event => {
+    
+ switch (event.keyCode) {
+
+  case 37:
+    turnLeft(rover);
+    break;
+  case 39:
+    turnRight (rover);
+    break;
+  case 70:
+    moveForward (rover);
+    break;
+ }
+
+
+});
+
+document.getElementById ("black_rover").addEventListener ("click", event => { changeRover ("black_rover")});
+document.getElementById ("red_rover").addEventListener ("click", event => { changeRover ("red_rover")});
+document.getElementById ("green_rover").addEventListener ("click", event => { changeRover ("green_rover")});
+
+
+
+
+initializeGrid (10, 10);
 update();
+
+placeRover (rover1);
+placeRover (rover2);
+placeRover (rover3);
+placeObstacles ();
 
 
 function update (time=0)
 
 {
 
-initializeGrid (10, 10);
+
 placeRover (rover);
 requestAnimationFrame (update);
 
+}
+
+function changeRover (roverColor)
+
+{
+
+  console.log ("rover changed");
+
+  switch (roverColor) {
+
+    case "black_rover":
+      rover=rover1;
+      break;
+   
+    case "red_rover":
+      rover=rover2;
+      break;
+
+    case "green_rover":
+      rover=rover3;
+      break;
+
+  }
 }
 
 
 function initializeGrid (numberOfRows, numberOfColumns)
 
 {
+  
   document.getElementById("main_grid").innerHTML="";
 
     for (i=0; i<= (numberOfRows-1); i++)
@@ -52,9 +127,79 @@ function placeRover (rover)
 {
 
   cellToPlaceRover = document.getElementById ("x:"+rover.x+"y:"+rover.y);
-  cellToPlaceRover.style.backgroundImage="url('../Ironhackexercise3/images/rover-icon-mars-" + rover.direction +".png";
+  cellToPlaceRover.style.backgroundImage="url('../Ironhackexercise3/images/rover-icon-mars-" + rover.color + "-" + rover.direction +".png";
   cellToPlaceRover.style.backgroundSize="cover";
+
+  if ((rover.travelLog.length>=2) && (!((rover.travelLog [rover.travelLog.length-1] [0] == rover.x) && (rover.travelLog [rover.travelLog.length-1] [1] == rover.y))))
+
+   {
  
+    oldCellToRemoveRover=document.getElementById ("x:"+rover.travelLog [rover.travelLog.length-1] [0]+"y:"+rover.travelLog [rover.travelLog.length-1] [1]);
+    oldCellToRemoveRover.style.backgroundImage="";
+
+    }
+
+}
+
+function placeObstacles () {
+
+  obstacles.forEach ((x, index) => {
+
+  cellToPlaceRover = document.getElementById ("x:"+obstacles [index] [0] +"y:"+obstacles [index] [1]);
+  cellToPlaceRover.style.backgroundImage="url('../Ironhackexercise3/images/obstacle.png')";
+  cellToPlaceRover.style.backgroundSize="cover";
+
+  });
+
+}
+
+function isAnotherRoverOnTheWay (roverForwardCoordinateX, roverForwardCoordinateY)
+
+{
+  let collision =0;
+
+  switch (rover) {
+
+    case rover1:
+      if (((rover2.x==roverForwardCoordinateX) && (rover2.y==roverForwardCoordinateY)) || ((rover3.x==roverForwardCoordinateX) && (rover3.y==roverForwardCoordinateY))) {collision++;}
+    case rover2:
+      if (((rover1.x==roverForwardCoordinateX) && (rover1.y==roverForwardCoordinateY)) || ((rover2.x==roverForwardCoordinateX) && (rover2.y==roverForwardCoordinateY))) {collision++;}
+    case rover3:
+      if (((rover1.x==roverForwardCoordinateX) && (rover1.y==roverForwardCoordinateY)) || ((rover2.x==roverForwardCoordinateX) && (rover2.y==roverForwardCoordinateY))) {collision++;}
+      
+
+  }
+
+  if (collision>0) { return true; } else { return false; }
+
+}
+
+
+
+
+function isObstacleOnTheWay (roverForwardCoordinateX, roverForwardCoordinateY)
+
+{
+
+  let collision=0;
+  
+  obstacles.forEach ((obstacle, index) => {
+
+
+    if  ((obstacle [0]==roverForwardCoordinateX) && (obstacle [1]==roverForwardCoordinateY)) 
+
+      {
+            
+       collision++
+      console.log ("collision at ("+ obstacle [0] +","+roverForwardCoordinateX +" and " + "collision at ("+ obstacle [1] +","+roverForwardCoordinateY);
+         
+      }
+
+
+  });
+
+  console.log ("collision is " + collision);
+  if (collision>0) { return true; } else { return false; }
 
 }
 
@@ -120,19 +265,35 @@ function moveForward(rover){
   switch (rover.direction) {
   
     case "N":
-      if ((rover.y)-1 >= 0) { rover.y=(rover.y)-1;}
-      break;
+
+        if (((rover.y)-1 >= 0) && (!isObstacleOnTheWay (rover.x, rover.y-1)) && !(isAnotherRoverOnTheWay (rover.x, rover.y-1)))
+        { 
+          rover.y=(rover.y)-1;
+        }
+        break;
 
     case "E":
-      if ((rover.x)-1>=0) { rover.x=(rover.x)-1 };
+     
+      if (((rover.x)-1>=0)  &&!(isObstacleOnTheWay (rover.x-1, rover.y)) && !(isAnotherRoverOnTheWay (rover.x-1, rover.y)))
+      { 
+        rover.x=(rover.x)-1; 
+      }
       break;
 
     case "S":
-      if ((rover.y)+1 <= y_boundary) { rover.y=(rover.y)+1 } ;
+      
+      if (((rover.y)+1 < y_boundary) &&!(isObstacleOnTheWay (rover.x, rover.y+1)) & !(isAnotherRoverOnTheWay (rover.x, rover.y+1)))
+      { 
+        rover.y=(rover.y)+1; 
+      }
       break;
 
     case "W":
-      if ((rover.x)+1 <= x_boundary) { rover.x=(rover.x)+1 };
+    
+  
+    if (((rover.x)+1 < x_boundary)   &&!(isObstacleOnTheWay (rover.x+1, rover.y)) && !(isAnotherRoverOnTheWay (rover.x+1, rover.y)))
+    { rover.x=(rover.x)+1; }
+
       break;
  
   }
